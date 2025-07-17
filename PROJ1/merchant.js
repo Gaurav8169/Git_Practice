dffcbdc89fd665b9e719285ed9b1a3d004cfceea
Gaurav8169 <gauravmishra8169@gmail.com>
2025-07-17 18:16:46 +0530

    merge branch first commit

--- PROJ1/merchant.js
+++ PROJ1/merchant.js
@@ -3,3 +3,7 @@ for (let i = 1; i <= 5; i++) {
   a = a + i;
 }
 console.log("sum of the first 5 number is :", a);
+
+setTimeout(() => {
+  console.log("this is the set time out function");
+}, 2000);
